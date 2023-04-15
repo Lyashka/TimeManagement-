fd0934c562e048dc1ed93ccdfcedde27898aa0a2
Lyashka <marzlyaha@mail.ru>
2023-04-15 11:37:11 +0300

    userController

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,7 +8,36 @@ class UserController {
     }
 
     async getUser(req, res) {
+        const {email, password} = req.body
 
+        const user = await db.query(`SELECT DISTINCT * FROM users
+        WHERE  users.email = $1 and users.password = $2 `, [email, password])
+        
+        const to_do_list = await db.query(`SELECT DISTINCT to_do_list_id, date_start, list_name FROM users
+        JOIN to_do_list ON users.user_id = to_do_list.user_id
+        WHERE  users.email = $1 and users.password = $2`, [email, password])
+
+        const content = await db.query(`SELECT DISTINCT case_content_id, content, completed_status, date_completed, id FROM case_content
+        JOIN to_do_list ON case_content.case_content_id = to_do_list.to_do_list_id
+        JOIN users ON users.user_id = to_do_list.user_id
+        WHERE  users.email = $1 and users.password = $2`, [email, password])
+        
+        user.rows[0]['to_do_list'] = to_do_list.rows
+        
+        user.rows[0].to_do_list.forEach((e) => {
+            e['content'] = []
+        })
+
+        content.rows.forEach((item) => {
+            user.rows[0].to_do_list.forEach((e) => {
+                if (e.to_do_list_id == item.case_content_id) {
+                    e.content.push(item)
+                }
+            })
+  
+        })
+
+        res.json(user.rows[0])
     }
 
     async updateUser(req, res) {
@@ -18,6 +47,43 @@ class UserController {
     }
 
     async deleteUser(req, res) {
+        const id = req.params.id
+        const user = await db.query(`DELETE FROM users WHERE user_id = $1`, [id])
+        res.json(user.rows[0])
+    }
+
+    async createToDoList(req, res) {
+        const {date_start, list_name, user_id} = req.body
+        const new_to_do_list = await db.query(`INSERT INTO to_do_list(date_start, list_name, user_id)
+        VALUES($1, $2, $3 )`, [date_start, list_name, user_id])
+        res.json(new_to_do_list.rows[0])
+    }
+
+    async updateToDoList(req, res) {
+        const {date_start, list_name, user_id, to_do_list_id} = req.body
+        const user = await db.query(`UPDATE to_do_list set date_start = $1, list_name = $2, user_id = $3 where to_do_list_id = $4 RETURNING *`, [date_start, list_name, user_id, to_do_list_id])
+        res.json(user.rows[0])
+    }
+ 
+    async createContent(req, res) {
+        const {case_content_id, content, completed_status, date_completed} = req.body
+        const new_content = await db.query(`INSERT INTO case_content(case_content_id, content, completed_status, date_completed)
+        VALUES($1, $2, $3, $4)`, [case_content_id, content, completed_status, date_completed])
+        res.json(new_content.rows[0])
+    }
+
+    async updateContent(req, res) {
+        const {content, completed_status, id} = req.body
+        const updateContent = await db.query(`UPDATE case_content
+        set content = $1, completed_status = $2 where id = $3`, [content, completed_status, id])
+        res.json(updateContent.rows[0])
+    }
+
+    async createPurpose(req, res) {
+
+    }
+
+    async updatePurpose(req, res) {
 
     }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,12 @@ const router = new Router()
 const userController = require('../controller/user.controller')
 
 router.post('/user', userController.createUser)
-router.get('/user/:id', userController.getUser)
+router.get('/user', userController.getUser)
 router.put('/user', userController.updateUser)
 router.delete('/user/:id', userController.deleteUser)
+router.post('/user/new-to-do-list', userController.createToDoList)
+router.put('/user/new-to-do-list', userController.updateToDoList)
+router.post('/user/new-content', userController.createContent)
+router.put('/user/new-content', userController.updateContent)
 
 module.exports = router
